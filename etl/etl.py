52923a5ef0f3318fbfc5e25429476b3fc155cfe2
Felipe Limeira <limeira.felipe94@gmail.com>
2023-07-06 21:01:28 -0300

    Adição do 'blue' e 'isort' para formatação do projeto, e separado em pastas para melhorar a estrutura

--- etl/etl.py
+++ etl/etl.py
@@ -1,12 +1,13 @@
+import os
+
 import cx_Oracle
 import pandas as pd
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-class ExcelToOracleETL:
 
+class ExcelToOracleETL:
     def __init__(self, fp):
         self.file_path = fp
 
@@ -15,20 +16,23 @@ class ExcelToOracleETL:
             df = pd.read_excel(self.file_path)
             return df
         except Exception as e:
-            print(f"Erro ao carregar o arquivo Excel: {str(e)}")
+            print(f'Erro ao carregar o arquivo Excel: {str(e)}')
             return None
 
     @staticmethod
     def connect_oracle():
         try:
             conn = cx_Oracle.connect(
-                user=os.getenv("DB_USERNAME"),
-                password=os.getenv("DB_PASSWORD"),
-                dsn=os.getenv("DB_HOST"),
-                encoding="UTF-8")
+                user=os.getenv('DB_USERNAME'),
+                password=os.getenv('DB_PASSWORD'),
+                dsn=os.getenv('DB_HOST'),
+                encoding='UTF-8',
+            )
             return conn
         except Exception as e:
-            raise ConnectionError(f"Não foi possível conectar ao banco de dados - {e}")
+            raise ConnectionError(
+                f'Não foi possível conectar ao banco de dados - {e}'
+            )
 
     def insert_data(self, df):
         try:
@@ -44,18 +48,18 @@ class ExcelToOracleETL:
             return True
 
         except Exception as e:
-            print(f"Erro ao inserir os dados no banco de dados Oracle: {str(e)}")
+            print(
+                f'Erro ao inserir os dados no banco de dados Oracle: {str(e)}'
+            )
             return False
 
 
 def main():
-    fp = "./dados/dados_mercado.xlsx"
+    fp = './dados/dados_mercado.xlsx'
     etl = ExcelToOracleETL(fp)
     df = etl.load_excel()
     etl.insert_data(df)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
-
-
--- tests/test_etl.py
+++ tests/test_etl.py
@@ -1,20 +1,25 @@
 import pandas as pd
 import pytest
-from etl import ExcelToOracleETL
+
+from etl.etl import ExcelToOracleETL
+
 
 @pytest.fixture()
 def etl():
     return ExcelToOracleETL
 
+
 def test_load_excel():
-    file_path = "./dados/dados_mercado.xlsx"
+    file_path = './dados/dados_mercado.xlsx'
     etl = ExcelToOracleETL(file_path)
     result = etl.load_excel()
     assert result is not None
 
+
 def test_connection(etl):
     result = etl.connect_oracle()
     assert result
+
 
 def test_insert_data_oracle():
     data = {'codigo': ['A', 'B', 'C'], 'quantidade': [1, 2, 3]}
@@ -22,5 +27,3 @@ def test_insert_data_oracle():
     etl = ExcelToOracleETL(df)
     result = etl.insert_data(df)
     assert result is True
-
-
